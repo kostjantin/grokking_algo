650d2e999cfe4487ac7b9625054d6f8979ab5678
Kostjantin <kostjantin@gmail.com>
2024-08-26 09:29:13 +0300

    - refactor

--- php/sort/quicksort.simple.php
+++ php/sort/quicksort.simple.php
@@ -4,18 +4,19 @@
  * @param array<int> $a
  * @return array<int>
  */
-function quicksort(array $a) {
+function quicksort(array $a)
+{
     $a_len = count($a);
     if ($a_len < 2) {
         return $a;
-    } elseif($a_len === 2) {
-	return ($a[0] > $a[1]) ? [$a[1], $a[0]] : $a;
+    } elseif ($a_len === 2) {
+        return ($a[0] > $a[1]) ? [$a[1], $a[0]] : $a;
     }
 
     $pivot = $a[rand(0, $a_len - 1)];
-	
+
     $first_part = $equal_part = $second_part = [];
-    for($i = 0; $i < $a_len; $i++) {
+    for ($i = 0; $i < $a_len; $i++) {
         if ($a[$i] < $pivot) {
             $first_part[] = $a[$i];
         } elseif ($a[$i] === $pivot) {
@@ -26,8 +27,8 @@ function quicksort(array $a) {
     }
 
     return array_merge(
-      quicksort($first_part),
-      quicksort($equal_part),
-      quicksort($second_part),
+        quicksort($first_part),
+        quicksort($equal_part),
+        quicksort($second_part),
     );
 }
